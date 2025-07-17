e8659971b894dd766b0b7c51bff5c47a749f6d3d
LoganLv <logan.lv.xmiao@gmail.com>
2025-07-18 05:03:14 +0800

    Init Project

--- src/App.vue
+++ src/App.vue
@@ -3,83 +3,40 @@ import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
-<template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
+<template data-bs-theme="dark">
+  <HelloWorld :msg="'Welcome to Your Vue.js App'" />
   <RouterView />
+
+  <nav class="navbar fixed-bottom navbar-light bg-light">
+    <div class="container-fluid">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <RouterLink to="/">
+            <i class="bi bi-speedometer2 me-auto"></i>
+            <div class="small">首页</div>
+          </RouterLink>
+        </li>
+        <li class="nav-item">
+          <RouterLink to="/tasks">
+            <i class="bi bi-currency-exchange me-auto"></i>
+            <div class="small">币币</div>
+          </RouterLink>
+        </li>
+        <li class="nav-item">
+          <RouterLink to="/wallet">
+            <i class="bi bi-arrows-angle-contract me-auto"></i>
+            <div class="small">合约</div>
+          </RouterLink>
+        </li>
+        <li class="nav-item">
+          <RouterLink to="/profile">
+            <i class="bi bi-wallet2 me-auto"></i>
+            <div class="small">资产</div>
+          </RouterLink>
+        </li>
+      </ul>
+    </div>
+  </nav>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+<style scoped lang="scss"></style>
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
 import './assets/main.css'
-
+import 'bootstrap'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
 
